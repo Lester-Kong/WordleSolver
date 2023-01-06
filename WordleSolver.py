25a1cce610c009234a347d6c5013bf1256f18c5e
Lester Kong <44414749+OpSeaLion@users.noreply.github.com>
2023-01-06 14:59:14 -0500

    Add files via upload
    added correctLetters function
    updated removeWord function to accommodate for correct letters
    added wordScore function

--- WordleSolver.py
+++ WordleSolver.py
@@ -7,12 +7,35 @@ def wrongLetters(result, guess):
             wrong_letters.append(guess[i])
     return wrong_letters
 
+def partialLetters(result, guess):
+    partial_letters = {}
+    for i in range(5):
+        if result[i] == "y":
+            partial_letters.update({guess[i]: i})
+    return partial_letters
+
+def correctLetters(result, guess):
+    correct_letters = {}
+    for i in range(5):
+        if result[i] == "g":
+            correct_letters.update({guess[i]:i})
+    return correct_letters
+
 def removeWord(possible_words, result, guess):
     wordsToRemove = []
     wrong_letters = wrongLetters(result, guess)
+    partial_letters = partialLetters(result, guess)
+    correct_letters = correctLetters(result, guess)
     for wrongletter in wrong_letters:
         for word in possible_words:
             if wrongletter in word and word not in wordsToRemove:
+                wordsToRemove.append(word)
+
+    for x in correct_letters:
+        for word in possible_words:
+            if x not in word and word not in wordsToRemove:
+                wordsToRemove.append(word)
+            if x in word and word[correct_letters[x]] != x and word not in wordsToRemove:
                 wordsToRemove.append(word)
             
     for word in wordsToRemove:
@@ -20,19 +43,33 @@ def removeWord(possible_words, result, guess):
         
     return possible_words
 
+def wordScore(word, letterFreq):
+    score = 0
+    for i in range(5):
+        score += letterFreq[word[i]][i]
+    return score
+
+def bestWord(possible_words, letterFreq):
+    bestWordScore = 0
+    for word in possible_Words:
+        nextWordScore = 0
+        for i in range(5):
+            nextWordScore += letterFreq[word[i]]
+
 def letterFreq(possible_words):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     letterFreqArr = {}
-    freq = [0,0,0,0,0]
+
     for letter in alphabet:
+        freq = [0,0,0,0,0]
         for word in possible_words:
             for i in range(5):
                 if word[i] == letter:
                     freq[i] += 1
         letterFreqArr.update({letter: freq})
-    return arr
+    return letterFreqArr
 
 """def wordleSolver(possible_words):
     print(wrongLetters("wwwww", "abcde"))"""
 
-print(removeWord(possible_words, "wwwww", "cigar"))
+print(removeWord(possible_words, "wwwgw", "baked"))
